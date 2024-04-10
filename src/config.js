4b724c4417d689ee75f8bee6c8b69ccc2d6b839c
Julio A <50281349+velcifer@users.noreply.github.com>
2024-04-09 23:48:03 -0500

    feature/update-variables-entorno

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DATABASE_HOST || "bdconectionaws.cve66ss4ej2r.us-east-2.rds.amazonaws.com",
-  user: process.env.DATABASE_USER || "admin",
+  host: process.env.DATABASE_HOST || "icom01.mysql.database.azure.com",
+  user: process.env.DATABASE_USER || "admin23",
   password: process.env.DATABASE_PASSWORD || "dXAraoB4C$&5",
   database: process.env.DATABASE_NAME || "linksdb",
   port: process.env.DATABASE_PORT || 3306,
